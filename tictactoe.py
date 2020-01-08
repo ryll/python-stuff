039064770ba4eb3484bfff80f48c2a267e9d5664
ryll <ryll.aman@gmail.com>
2020-01-08 15:33:42 +0000

    TicTacToe update

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 empty_board =[' ']*9
+playing = True
 
 def print_board(board):
     print('[{}]\n[{}]\n[{}]'.format(']['.join(board[:3]),']['.join(board[3:6]),']['.join(board[6:])))
@@ -24,14 +25,14 @@ def update_board(board,player,play):
 
 def check_win(board):
   wins = [{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}]
-  plays = [set([n for n,x in enumerate(board) if x == 'X']),set([n for n,x in enumerate(board) if x == 'X'])]
+  plays = [set([n for n,x in enumerate(board) if x == 'X']),set([n for n,x in enumerate(board) if x == 'O'])]
   for w in wins:
-    if w <= plays[0] or w <= plays[1]
+    if w <= plays[0] or w <= plays[1]:
+      return True
+  return False
   
-  return any((i in w for i in plays))
-
-def play():
-  board = empty_board
+while playing:
+  board = empty_board.copy()
   player = 1
   for n in range(9):
     print_board(board)
@@ -40,14 +41,12 @@ def play():
     if n>3 and check_win(board):
       print_board(board)
       print('Player {} won!'.format(player))
-      play_again = input('Do you want to play again? (Y/N): ').upper() == 'Y'
+      if input('Do you want to play again? (Y/N): ').upper() == 'N':
+        playing = False
+        break
     player ^= 0b11
   else:
     print_board(board)
     print('No winner.')
-    play_again = input('Do you want to play again? (Y/N): ').upper() == 'Y'
-  
-  if play_again:
-    play()
-
-play()
+    if input('Do you want to play again? (Y/N): ').upper() == 'N':
+      playing = False
