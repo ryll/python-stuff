0391cbb6e58a0a58d1bd0b09be63bea1b721daa7
ryll <ryll.aman@gmail.com>
2020-01-12 17:26:36 +0000

    message

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ suits = ('Hearts', 'Spades', 'Diamonds', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,'Queen':10, 'King':10, 'Ace':11}
 
+
 class Deck():
   """A deck of cards for BlackJack."""
   base_setup = [2,3,4,5,6,7,8,9,10,'Knight','Queen','King','Ace']
